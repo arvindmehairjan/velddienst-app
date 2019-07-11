5decfe14ef4138b6e843b5b657b588cb4da48f36
Arvind <Arvind>
2019-07-11 15:13:05 +0200

    Added new parameters in employee model interface

--- frontend/src/app/employee.model.ts
+++ frontend/src/app/employee.model.ts
@@ -2,6 +2,14 @@ export interface Employee {
   id: String;
   firstName: String;
   lastName: String;
+  address: String;
+  telephone: Number;
+  email: String;
   employeeId: Number;
   group: Number;
+  verspreiding: Number;
+  film: Number;
+  nabezoek: Number;
+  bijbelstudie: Number;
+  overige: String;
 }

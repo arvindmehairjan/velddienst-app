fc3a5389fc89876b67e641e16dbfc5a0f45f1ee2
Arvind <Arvind>
2019-07-11 15:23:18 +0200

    Added new parameters to employee service

--- frontend/src/app/employee.service.ts
+++ frontend/src/app/employee.service.ts
@@ -18,22 +18,38 @@ export class EmployeeService {
     return this.http.get(`${this.url}/employees/${id}`);
   }
 
-  addEmployee(firstName, lastName, employeeId, group) {
+  addEmployee(firstName, lastName, address, telephone, email, employeeId, group, verspreiding, film, nabezoek, bijbelstudie, overige) {
     const employee = {
       firstName: firstName,
       lastName: lastName,
+      address: address,
+      telephone: telephone,
+      email: email,
       employeeId: employeeId,
-      group: group
+      group: group,
+      verspreiding: verspreiding,
+      film: film,
+      nabezoek: nabezoek,
+      bijbelstudie: bijbelstudie,
+      overige: overige
     };
     return this.http.post(`${this.url}/employees/add`, employee);
   }
 
-  updateEmployee(id, firstName, lastName, employeeId, group) {
+  updateEmployee(id, firstName, lastName, address, telephone, email, employeeId, group, verspreiding, film, nabezoek, bijbelstudie, overige) {
     const employee = {
       firstName: firstName,
       lastName: lastName,
+      address: address,
+      telephone: telephone,
+      email: email,
       employeeId: employeeId,
-      group: group
+      group: group,
+      verspreiding: verspreiding,
+      film: film,
+      nabezoek: nabezoek,
+      bijbelstudie: bijbelstudie,
+      overige: overige
     };
     return this.http.post(`${this.url}/employees/update/${id}`, employee);
   }
